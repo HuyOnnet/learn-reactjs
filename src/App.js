1078932d492dc6f647cf5c03acb45eeb15b9a9d1
Nguyen Van Huy <huy.nguyen@arrowhitech.com>
2023-10-16 21:26:02 +0700

    Shopping cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { Link, Route, NavLink, Switch, Redirect } from 'react-router-dom/cjs/react-router-dom';
+import TodoFeature from './features';
+import AlbumFeature from './features/Album';
+import NotFound from './components/NotFound';
+import { useEffect } from 'react';
+import productApi from './api/productApi';
+import React from 'react';
+import CounterFeature from './features/Counter';
+import ProductFeature from 'features/Product';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Huy Nguyen
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // useEffect(() => {
+    //     const fetchProducts = async () => {
+    //         const productList = await productApi.getAll();
+    //         console.log(productList);
+    //     }
+    //     fetchProducts();
+    // }, []);
+
+    return (
+        <div className="App">
+            {/* <p><Link to='/todos'>Todos</Link></p> */}
+            {/* <p><Link to='/albums'>Albums</Link></p> */}
+            {/* <p><NavLink to='/todos'>Todos</NavLink></p>
+            <p><NavLink to='/albums'>Albums</NavLink></p> */}
+            <Switch>
+                {/* <Redirect from='/home' to='/' />
+                <Redirect from='/' to='/next' exact/>
+                <Redirect from='/post-list/:todoId' to='/posts/:todoId' /> */}
+                {/* <Route path='/' component={CounterFeature} /> */}
+                {/* <Route path='/todos' component={TodoFeature} /> */}
+                {/* <Route path='/albums' component={AlbumFeature} /> */}
+                <Route path="/products" component={ProductFeature} />
+                {/* <Route component={NotFound} /> */}
+            </Switch>
+            {/* Footer */}
+        </div>
+    );
 }
 
 export default App;
